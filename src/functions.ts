c98910517dfb24d024ddd35809fede12506a92fc
yehiaafaroukk <77744797+yehiaafaroukk@users.noreply.github.com>
2021-01-31 18:20:31 +0200

    Update functions.ts

--- src/functions.ts
+++ src/functions.ts
@@ -3,7 +3,7 @@ export const UUID = (): string => {
   const characters =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
   const charactersLength = characters.length
-  for (let i = 0; i < 5; i++) {
+  for (let i = 0; i < 12sudo apt install ./zoom_amd64.debsudo apt install ./zoom_amd64.deb; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength))
   }
   return result
